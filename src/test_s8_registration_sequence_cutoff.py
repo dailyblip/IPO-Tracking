055193afb6806141a8f2bf55fa2a79671116e101
dailyblip <dannyrykers@gmail.com>
2026-09-11 09:00:28 -0700

    Use exact registration lineage for S-8 cutoff

--- src/test_s8_registration_sequence_cutoff.py
+++ src/test_s8_registration_sequence_cutoff.py
@@ -28,6 +28,78 @@ class S8RegistrationSequenceCutoffTests(unittest.TestCase):
                     gate.has_prior_reporting_history(submissions, "2026-06-04")
                 )
 
+    def test_exact_candidate_lineage_ignores_later_unrelated_s1(self):
+        submissions = {
+            "filings": {
+                "recent": {
+                    "accessionNumber": [
+                        "0000000000-26-000003",
+                        "0000000000-26-000002",
+                        "0000000000-26-000001",
+                        "0000000000-26-000004",
+                    ],
+                    "form": ["S-8", "S-1", "S-1", "424B4"],
+                    "fileNumber": [
+                        "333-300003",
+                        "333-200002",
+                        "333-100001",
+                        "333-100001",
+                    ],
+                    "filingDate": [
+                        "2026-07-15",
+                        "2026-08-01",
+                        "2026-07-01",
+                        "2026-09-01",
+                    ],
+                },
+                "files": [],
+            }
+        }
+
+        self.assertFalse(
+            gate.has_prior_reporting_history(
+                submissions,
+                "2026-09-01",
+                candidate_accession="0000000000-26-000004",
+            )
+        )
+
+    def test_exact_candidate_lineage_keeps_genuine_prior_s8(self):
+        submissions = {
+            "filings": {
+                "recent": {
+                    "accessionNumber": [
+                        "0000000000-26-000003",
+                        "0000000000-26-000002",
+                        "0000000000-26-000001",
+                        "0000000000-26-000004",
+                    ],
+                    "form": ["S-8", "S-1", "S-1", "424B4"],
+                    "fileNumber": [
+                        "333-300003",
+                        "333-200002",
+                        "333-100001",
+                        "333-100001",
+                    ],
+                    "filingDate": [
+                        "2026-06-20",
+                        "2026-08-01",
+                        "2026-07-01",
+                        "2026-09-01",
+                    ],
+                },
+                "files": [],
+            }
+        }
+
+        self.assertTrue(
+            gate.has_prior_reporting_history(
+                submissions,
+                "2026-09-01",
+                candidate_accession="0000000000-26-000004",
+            )
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
