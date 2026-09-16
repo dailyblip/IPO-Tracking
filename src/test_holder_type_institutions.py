4ebb78dc676e59a9ef0457aa7feb0b216680e54b
dailyblip <dannyrykers@gmail.com>
2026-09-16 07:03:57 -0700

    Classify L.P holders without trailing period

--- src/test_holder_type_institutions.py
+++ src/test_holder_type_institutions.py
@@ -31,6 +31,10 @@ class InstitutionalHolderTypeTests(unittest.TestCase):
         self.assertEqual(holder_type("OGGI Equity Srl."), "Entity")
         self.assertEqual(holder_type("Example S.r.l."), "Entity")
 
+    def test_lp_without_trailing_period_is_fund(self):
+        self.assertEqual(holder_type("OCM Power VI AIV Holdings (Delaware), L.P"), "Fund")
+        self.assertEqual(holder_type("Example Investment L.P"), "Fund")
+
     def test_plural_fund_labels_are_not_people(self):
         self.assertEqual(holder_type("MDP Funds"), "Fund")
         self.assertEqual(holder_type("Example Healthcare Funds"), "Fund")
