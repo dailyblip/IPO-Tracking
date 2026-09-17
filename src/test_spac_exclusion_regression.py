68ea014c0c179af52d9eda50aab6f54a7a8b727b
dailyblip <dannyrykers@gmail.com>
2026-09-16 21:58:01 -0700

    Exclude plural Acquisitions SPAC issuer names

--- src/test_spac_exclusion_regression.py
+++ src/test_spac_exclusion_regression.py
@@ -7,6 +7,7 @@ def test_known_spac_franchise_names_are_excluded():
     assert edgar_client.check_spac_indicators("", company_name="Gores Holdings XII, Inc.")
     assert edgar_client.check_spac_indicators("", company_name="GigCapital10 Corp.")
     assert edgar_client.check_spac_indicators("", company_name="Graf Industrial Corp. II")
+    assert edgar_client.check_spac_indicators("", company_name="Lower Cross Acquisitions Corp")
 
 
 def test_operating_company_holdings_name_is_not_blanket_excluded():
